7ce61830f9bb43b0baf66b8126b289e0919a86a5
MisaBackEnd <misaelriot@hotmail.com>
2024-07-27 00:30:00 -0500

    [add] added conditional directives component

--- fundamentos/helloworld/src/App.vue
+++ fundamentos/helloworld/src/App.vue
@@ -4,9 +4,11 @@ import PostContainer from './components/PostContainer.vue';
 import CounterOption from './components/CounterOption.vue';
 import CounterComposition from './components/CounterComposition.vue';
 import CounterCompositionWithSetupKw from './components/CounterCompositionWithSetupKw.vue';
+import ConditionalDirectives from './components/ConditionalDirectives.vue';
 </script>
 
 <template>
+  <ConditionalDirectives />
   <h1>Listado de Post:</h1>
   <PostContainer />
   <h2>Counter:</h2>
