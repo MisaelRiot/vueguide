dde7702c6b6c01c479fe6761f8db7ffeb27c046c
MisaBackEnd <misaelriot@hotmail.com>
2024-08-18 19:33:36 -0500

    [add] added services for different api configs

---
+++ fundamentos/VueServicesPractice/src/components/PostListWithOption.vue
@@ -0,0 +1,34 @@
+<script>
+    import { defineComponent, ref } from 'vue';
+    import PostService from '@/services/PostService';
+    
+    export default defineComponent({
+        name: "PostListWithOption",
+        data(){
+            // utilizamos el ciclo de vida del componente para llamar
+            // a nuestros servicios.
+            const posts = ref([])
+            return {posts}
+        },
+        async mounted(){
+            const service = new PostService()
+            this.posts = service.getPosts()
+            await service.fetchAll()
+            console.log(this.posts[0].title)
+        }
+    })
+</script>
+
+<template>
+    <div>
+        <h1>Listado de post</h1>
+        <p>Este componente es escrito con composition con option api</p>
+        <ul class="post-list">
+            <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ fundamentos/VueServicesPractice/src/components/PostListWithSetup.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import { onMounted } from 'vue';
+    import PostService from '@/services/PostService';    
+
+    const service = new PostService()
+    const posts = service.getPosts()
+    onMounted(async () =>{
+        await service.fetchAll()
+    })
+
+</script>
+
+<template>
+    <div>
+        <h1>Listado de post</h1>
+        <p>Este componente es escrito con composition con setup</p>
+        <ul class="post-list">
+            <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
