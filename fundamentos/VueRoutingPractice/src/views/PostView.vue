7b38df53be562a992326dad17de79af08f1882ba
MisaBackEnd <misaelriot@hotmail.com>
2024-08-23 14:00:05 -0500

    [add] added how to access parameters

--- fundamentos/VueRoutingPractice/src/views/PostView.vue
+++ fundamentos/VueRoutingPractice/src/views/PostView.vue
@@ -1,5 +1,18 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+let postId = ref('')
+
+onMounted(() => {
+    const route = useRoute()
+    postId.value = route.params.id
+})
+
+</script>
+
 <template>
-    <div>
-        <h1>Post View</h1>
-    </div>
+    <h1>Post View del elemento: {{ postId }}</h1>
 </template>
+
+<style scoped></style>
