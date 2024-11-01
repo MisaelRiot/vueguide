a9542a2e07ce86caed584332123b5db384858f50
MisaBackEnd <misaelriot@hotmail.com>
2024-11-01 08:38:25 -0500

    [add] added dynamic components with layouts

---
+++ fundamentos/dynamic/src/components/UserList.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps(['content'])
+</script>
+
+<template>
+    <div class="list">
+        <h3>{{ props.content.name }}</h3>
+        <p>Edad: {{ props.content.age }}</p>
+        <p>{{ props.content.position }}</p>
+    </div>
+</template>
+
+<style scoped>
+.list {
+    background-color: aquamarine;
+    border-radius: 10px;
+    padding: 10px;
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    margin: 10px;
+}
+</style>
